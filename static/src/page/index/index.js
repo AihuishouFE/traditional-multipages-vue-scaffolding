5bdab56709b189e06d00de7771c7d25c7fa949c2
dalston <xxdalston@gmail.com>
2017-05-07 21:50:22 +0800

    update v 0.0.2

--- static/src/page/index/index.js
+++ static/src/page/index/index.js
@@ -1,75 +1,13 @@
 // demo  
 
 new Vue({
-  el: '#app',
-  data: function(){
-  	var checkAge = (rule, value, callback) => {
-    if (!value) {
-      return callback(new Error('年龄不能为空'));
-    }
-    setTimeout(() => {
-      if (!Number.isInteger(value)) {
-        callback(new Error('请输入数字值'));
-      } else {
-        if (value < 18) {
-          callback(new Error('必须年满18岁'));
-        } else {
-          callback();
-        }
-      }
-    }, 1000);
-  };
-  var validatePass = (rule, value, callback) => {
-    if (value === '') {
-      callback(new Error('请输入密码'));
-    } else {
-      if (this.ruleForm2.checkPass !== '') {
-        this.$refs.ruleForm2.validateField('checkPass');
-      }
-      callback();
-    }
-  };
-  var validatePass2 = (rule, value, callback) => {
-    if (value === '') {
-      callback(new Error('请再次输入密码'));
-    } else if (value !== this.ruleForm2.pass) {
-      callback(new Error('两次输入密码不一致!'));
-    } else {
-      callback();
-    }
-  };
-  return {
-    ruleForm2: {
-      pass: '',
-      checkPass: '',
-      age: ''
+    el: '#app',
+    data: function(){
+        return {
+            test: 'hello world'
+        };
     },
-    rules2: {
-      pass: [
-        { validator: validatePass, trigger: 'blur' }
-      ],
-      checkPass: [
-        { validator: validatePass2, trigger: 'blur' }
-      ],
-      age: [
-        { validator: checkAge, trigger: 'blur' }
-      ]
-    }
-  };
-  },
-  methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
+    methods: {
+
     }
 })
